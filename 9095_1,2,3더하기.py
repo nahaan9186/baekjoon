13cf82ebe4e0d2ebfa99352518f8cef806ab76bd
Haan Na <nahaan9186@gmail.com>
2024-08-23 01:06:23 +0900

    123더하기

---
+++ 9095_1,2,3더하기.py
@@ -0,0 +1,19 @@
+N = int(input())
+li = [int(input()) for _ in range(N)]
+dp = [0] * 12
+# print(li)
+# print(dp)
+# print(max(li))
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+
+for i in range(4,12):
+    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+
+
+for i in li:
+    print(dp[i])
+    
+
+
